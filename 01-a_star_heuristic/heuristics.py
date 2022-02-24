b8832021abf3ce27b78a3292ff2abdfaf52b412e
David Zeman <davca.zeman@gmail.com>
2022-02-24 17:43:04 +0100

    Prvni 4

--- 01-a_star_heuristic/heuristics.py
+++ 01-a_star_heuristic/heuristics.py
@@ -10,16 +10,24 @@ def grid_2D_heuristic(current, destination):
     return abs((current[0]-destination[0])) + abs((current[1]-destination[1]))
 
 def grid_diagonal_2D_heuristic(current, destination):
-    return 0
+    return max(abs((current[0]-destination[0])), abs((current[1]-destination[1])))
 
 def grid_3D_heuristic(current, destination):
-    return 0
-
-def grid_face_diagonal_3D_heuristic(current, destination):
-    return 0
+    return abs((current[0]-destination[0])) + abs((current[1]-destination[1])) + abs((current[2]-destination[2]))
 
 def grid_all_diagonal_3D_heuristic(current, destination):
-    return 0
+    return max(abs((current[0]-destination[0])), abs((current[1]-destination[1])), abs((current[2]-destination[2])))
+
+def grid_face_diagonal_3D_heuristic(current, destination):
+    val = 0
+    if current[0] != destination[0] and current[1] != destination[1] and current[2] != destination[2]:
+        val = max(
+            max(abs((current[0]-destination[0])), abs((current[1]-destination[1])))
+            ,max(abs((current[0]-destination[0])), abs((current[2]-destination[2])))
+            ,max(abs((current[1]-destination[1])), abs((current[2]-destination[2]))))
+    else: 
+        val = max(abs((current[0]-destination[0])), abs((current[1]-destination[1])), abs((current[2]-destination[2])))
+    return val
 
 def grid_knight_2D_heuristic(current, destination):
-    return 0
+    return max(abs((current[0]-destination[0])) - abs((current[1]-destination[1])), abs((current[1]-destination[1])) - abs((current[0]-destination[0])))

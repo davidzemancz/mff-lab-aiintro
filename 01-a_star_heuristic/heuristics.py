3635c54f3bd1fdbc6743102eed6ece3836cc92fd
David Zeman <davca.zeman@gmail.com>
2022-02-27 14:40:26 +0100

    3D diagonal

--- 01-a_star_heuristic/heuristics.py
+++ 01-a_star_heuristic/heuristics.py
@@ -20,19 +20,15 @@ def grid_all_diagonal_3D_heuristic(current, destination):
 
 def grid_face_diagonal_3D_heuristic(current, destination):
     val = 0
-
     d0 = abs((current[0]-destination[0]))
     d1 = abs((current[1]-destination[1]))
     d2 = abs((current[2]-destination[2]))
-    
-    if d0 <= d1 and d0 <= d2:
-        val = d0 + max(d1, d2)
-    elif  d1 <= d2:
-        val = d1 + max(d0, d2) 
-    else:
-        val = d2 + max(d1, d0)
 
-    print(current, destination, val)
+    dmax = max(d0, d1,d2)
+    sum = d0 + d1 + d2
+
+    if sum - dmax <= dmax: val = dmax
+    else: val = math.ceil(sum / 2)
 
     return val
 
@@ -42,7 +38,12 @@ def grid_knight_2D_heuristic(current, destination):
     d0 = abs((current[0]-destination[0]))
     d1 = abs((current[1]-destination[1]))
 
-    val = min(d0,d1)
-    print(current, destination, val)
+    dmax = max(d0, d1)
+    dmin = min(d0, d1)
+
+    if dmin * 2 < dmax:
+        val = math.ceil(dmax / 2)
+    else:
+        val = math.ceil((dmax + dmin) / 3)
 
     return val

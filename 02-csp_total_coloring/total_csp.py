c2b49f1e59a377547bcdea098c4f67bc15c19613
David Zeman <davca.zeman@gmail.com>
2022-03-07 18:16:10 +0100

    Merge

--- 02-csp_total_coloring/total_csp.py
+++ 02-csp_total_coloring/total_csp.py
@@ -21,16 +21,56 @@ def total_coloring(graph):
             colors += 1
         return colors
     """
-    problem = constraint.Problem()
+    # http://labix.org/doc/constraint/
+    solutution = None
+    for colors in range(1, 100):
+        problem = constraint.Problem()
 
-    colors = 0
-    for u, v in graph.edges():
-        graph.edges[u,v]["color"] = colors
-        problem.addConstraint(lambda u, v: u["color"] != v["color"] , ("a", "b"))
+        # Add variables for vertices
+        for v in graph.nodes():
+            problem.addVariable(get_vrtx_name(v), range(0, colors))
 
-    print(problem.getSolutions())
+        # Add variables for edges
+        edges = {}
+        edge = 1000
+        for u,v in graph.edges():
+            problem.addVariable(edge, range(0, colors))
+            edges[get_edge_name(u,v)] = edge
+            edges[get_edge_name(v,u)] = edge
+            edge = edge + 1
+
+        # Add constrains
+        for u,v in graph.edges():
+            # Conected vertices
+            problem.addConstraint(lambda c1, c2: c1 != c2, (get_vrtx_name(u), get_vrtx_name(v)))
+
+            # Edge its and vertices
+            problem.addConstraint(lambda c1, c2: c1 != c2, (get_vrtx_name(v), edges[get_edge_name(u,v)]))
+
+            for n in graph.neighbors(u):
+                if n != v:
+                    problem.addConstraint(lambda c1, c2: c1 != c2, (edges[get_edge_name(u,n)], edges[get_edge_name(u,v)]))
+            
+            for n in graph.neighbors(v):
+                if n != u:
+                    problem.addConstraint(lambda c1, c2: c1 != c2, (edges[get_edge_name(v,n)], edges[get_edge_name(u,v)]))
+
+       
+        solutution = problem.getSolution()
+        if solutution is not None: break
     
+    print("Colors:", colors)
+    print("Solution:", solutution)
+    
+    if solutution is not None:
+        # Apply coloring
+        for v in graph.nodes():
+            graph.nodes[v]["color"] = solutution[get_vrtx_name(v)]
+        for u,v in graph.edges():
+            graph.edges[u,v]["color"] = solutution[edges[get_edge_name(u,v)]]
+
     return colors
 
    
-
+def get_vrtx_name(u): return u
+def get_edge_name(u, v): return str(u) + "-" + str(v)

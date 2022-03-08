eaf35496e9e8adf37a23d59e619119705de69ead
David Zeman <David.zeman@outlook.com>
2022-03-08 18:19:57 +0100

    STA

--- 03-sat_total_coloring/total_sat.py
+++ 03-sat_total_coloring/total_sat.py
@@ -9,14 +9,42 @@ def total_coloring(graph):
         Furthermore, assign property "color" for every vertex and edge. The value of the color has to be an integer between 0 and x-1.
 
         TODO: The implementation of this function finds some total coloring but the number of colors may be minimal.
-        Find the total chromatic index.
+        Find the total chromatic index
+        
+        for u in graph.nodes():
+            graph.nodes[u]["color"] = colors
+            colors += 1
+        for u,v in graph.edges():
+            graph.edges[u,v]["color"] = colors
+            colors += 1.
     """
-    colors = 0
-    for u in graph.nodes():
-        graph.nodes[u]["color"] = colors
-        colors += 1
-    for u,v in graph.edges():
-        graph.edges[u,v]["color"] = colors
-        colors += 1
+
+    # Get maximum vertex degree
+    max_deg = 1
+    for v in graph.nodes():
+        deg = graph.degree(v)
+        if deg > max_deg:
+            max_deg = deg
+
+    g = None
+    solved = False
+    colors = max_deg + 1
+    while not solved:
+        g = Glucose3()
+        #g.add_clause([-1, 2])
+
+
+
+        solved = g.solve()
+        if solved: break
+        else: colors = colors + 1
+
+    if solved:
+        model = g.get_model()
+    
     return colors
+
+def get_vrtx_name(u): return u
+def get_edge_name(u, v): return str(u) + "-" + str(v)
+
 
